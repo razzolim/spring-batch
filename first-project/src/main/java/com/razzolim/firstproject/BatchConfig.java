fc04ec4ad70ab2ce6c18ff712d1d06645b243e2c
Renan Azzolim <azzolimrenan2@gmail.com>
2022-11-18 11:33:56 -0300

    first batch job example

---
+++ first-project/src/main/java/com/razzolim/firstproject/BatchConfig.java
@@ -0,0 +1,46 @@
+package com.razzolim.firstproject;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing // this annotation allows that we inject and utilize components of the framework
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job printHelloJob() {
+        return jobBuilderFactory
+                .get("printHelloJob")
+                .start(printsHelloStep())
+                .build();
+    }
+
+    private Step printsHelloStep() {
+        return stepBuilderFactory
+                .get("printsHelloStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Hello, World!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+}
