edcb644f4a16b4e7471b249d184738f138d594a8
Renan Azzolim <azzolimrenan2@gmail.com>
2022-11-23 18:45:25 -0300

    reading multiple files

--- src/main/java/com/razzolim/batch/multipleformat/reader/FileClientTransactionReader.java
+++ src/main/java/com/razzolim/batch/multipleformat/reader/FileClientTransactionReader.java
@@ -3,13 +3,16 @@ package com.razzolim.batch.multipleformat.reader;
 import com.razzolim.batch.multipleformat.domain.Client;
 import com.razzolim.batch.multipleformat.domain.Transaction;
 import org.springframework.batch.item.*;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.ResourceAwareItemReaderItemStream;
+import org.springframework.core.io.Resource;
 
-public class FileClientTransactionReader implements ItemStreamReader<Client> {
+public class FileClientTransactionReader implements ItemStreamReader<Client>, ResourceAwareItemReaderItemStream<Client> {
 
     private Object actualObject;
-    private ItemStreamReader<Object> delegate;
+    private FlatFileItemReader<Object> delegate;
 
-    public FileClientTransactionReader(ItemStreamReader<Object> delegate) {
+    public FileClientTransactionReader(FlatFileItemReader<Object> delegate) {
         this.delegate = delegate;
     }
 
@@ -48,5 +51,10 @@ public class FileClientTransactionReader implements ItemStreamReader<Client> {
     @Override
     public void close() throws ItemStreamException {
         delegate.close();
+    }
+
+    @Override
+    public void setResource(Resource resource) {
+        delegate.setResource(resource);
     }
 }
